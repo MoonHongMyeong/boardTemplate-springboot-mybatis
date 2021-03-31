87f5e93c12e02a5ddb9c0ac6215c0b54fa251567
MoonHongMyeong <moonhongmyeong@gmail.com>
2021-03-31 18:53:13 +0900

    Refactor : Seperate message method => utils.Message Class

--- src/main/java/me/moon/boardTemplate/controller/UserController.java
+++ src/main/java/me/moon/boardTemplate/controller/UserController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import me.moon.boardTemplate.dto.user.*;
 import me.moon.boardTemplate.service.LoginService;
 import me.moon.boardTemplate.service.UserService;
+import me.moon.boardTemplate.utils.Message;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -31,21 +32,21 @@ public class UserController {
     @PostMapping()
     public ResponseEntity register(@RequestBody UserSaveRequestDto userSaveRequestDto) {
         userService.addUser(userSaveRequestDto);
-        return new ResponseEntity(message("회원가입 성공!"), HttpStatus.CREATED);
+        return new ResponseEntity(new Message("회원가입 성공!"), HttpStatus.CREATED);
     }
 
     //회원수정
     @PutMapping("/{userId}")
     public ResponseEntity modifyUser(@PathVariable("userId") Long userId, @RequestBody UserUpdateRequestDto userUpdateRequestDto) {
         userService.modifyUser(userId, userUpdateRequestDto);
-        return new ResponseEntity(message("수정을 완료했습니다."), HttpStatus.OK);
+        return new ResponseEntity(new Message("수정을 완료했습니다."), HttpStatus.OK);
     }
 
     //회원탈퇴
     @DeleteMapping("/{userId}")
     public ResponseEntity withdrawal(@PathVariable Long userId) {
         userService.deleteUser(userId);
-        return new ResponseEntity(message("회원탈퇴를 완료했습니다."), HttpStatus.OK);
+        return new ResponseEntity(new Message("회원탈퇴를 완료했습니다."), HttpStatus.OK);
     }
     //로그인
     @PostMapping("/login")
@@ -55,21 +56,16 @@ public class UserController {
         Optional<LoginUserDto> user = userService.findUserByEmailAndPassword(email, password);
         if (user.isPresent()) {
             loginService.loginUser(user.get().getEmail());
-            return new ResponseEntity(message("로그인 성공!"), HttpStatus.OK);
+            return new ResponseEntity(new Message("로그인 성공!"), HttpStatus.OK);
         } else {
-            return new ResponseEntity(message("로그인 실패 ㅜㅜ"), HttpStatus.NOT_FOUND);
+            return new ResponseEntity(new Message("로그인 실패 ㅜㅜ"), HttpStatus.NOT_FOUND);
         }
     }
     //로그아웃
     @GetMapping("/logout")
     public ResponseEntity logout() {
         loginService.logoutUser();
-        return new ResponseEntity(message("로그아웃하셨습니다."), HttpStatus.OK);
+        return new ResponseEntity(new Message("로그아웃하셨습니다."), HttpStatus.OK);
     }
 
-    private Map<String,String> message(String msg){
-        Map<String, String> message = new HashMap<>();
-        message.put("msg", msg);
-        return message;
-    }
 }
